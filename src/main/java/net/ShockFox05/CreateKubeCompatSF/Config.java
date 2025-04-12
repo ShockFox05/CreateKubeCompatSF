a1ae17c4873f600344e9b7058c223be9620fb3e8
ShockFox05 <dakotastodghill@gmail.com>
2025-04-11 23:53:56 -0400

    actual base

--- src/main/java/net/ShockFox05/CreateKubeCompatSF/Config.java
+++ src/main/java/net/ShockFox05/CreateKubeCompatSF/Config.java
@@ -1,12 +1,5 @@
 package net.ShockFox05.CreateKubeCompatSF;
 
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
